fa9f853b6518cf37a7aba3fbd3384cc4feb025c9
SebGeek <famille.auray@gmail.com>
2021-04-11 15:40:51 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
-function light_on (number_leds: number, red: number, green: number, blue: number) {
+function France () {
+    control_one_led(0, 0, 200)
+    control_one_led(0, 0, 200)
+    control_one_led(66, 66, 66)
+    control_one_led(66, 66, 66)
+    control_one_led(200, 0, 0)
+    control_one_led(200, 0, 0)
+    // Restart from first led index (flush)
+    control.waitMicros(500)
+}
+function control_one_led (red: number, green: number, blue: number) {
+    return_val = pins.spiWrite(red)
+    return_val = pins.spiWrite(green)
+    return_val = pins.spiWrite(blue)
+}
+function Italy () {
+    control_one_led(0, 200, 0)
+    control_one_led(0, 200, 0)
+    control_one_led(66, 66, 66)
+    control_one_led(66, 66, 66)
+    control_one_led(200, 0, 0)
+    control_one_led(200, 0, 0)
+    // Restart from first led index (flush)
+    control.waitMicros(500)
+}
+function control_leds (number_leds: number, red: number, green: number, blue: number) {
     for (let index = 0; index < number_leds; index++) {
-        return_val = pins.spiWrite(red)
-        return_val = pins.spiWrite(green)
-        return_val = pins.spiWrite(blue)
+        control_one_led(red, green, blue)
     }
     // Restart from first led index (flush)
     control.waitMicros(500)
@@ -12,18 +35,18 @@ let return_val = 0
 pins.spiFrequency(1000000)
 pins.spiFormat(8, 3)
 let total_number_leds = 96
-light_on(total_number_leds, 0, 0, 0)
+control_leds(total_number_leds, 0, 0, 0)
 // WS2801 LED strip  : definition : microbit
 // white             : GND        : GND
 // blue              : MOSI       : P15
 // green             : SCK        : P13
 // red               : VCC        : Not Connected
 basic.forever(function () {
-    led_count = 4
-    light_on(led_count, 0, 5, 0)
+    led_count = 6
     basic.clearScreen()
+    France()
     basic.pause(1000)
-    light_on(led_count, 0, 0, 5)
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Ghost)
+    Italy()
     basic.pause(1000)
 })
