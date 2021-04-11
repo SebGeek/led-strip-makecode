eba6db93a00d638eda34b0b65e46df7b3e97ae75
SebGeek <famille.auray@gmail.com>
2021-04-11 11:47:05 +0200

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-def light_on(number_leds: number, red: number, green: number, blue: number):
+def light_on2(number_leds: number, red: number, green: number, blue: number):
+    global return_val
     for index in range(number_leds):
-        pins.spi_write(red)
-        pins.spi_write(green)
-        pins.spi_write(blue)
+        return_val = pins.spi_write(red)
+        return_val = pins.spi_write(green)
+        return_val = pins.spi_write(blue)
+    # WS2801 needs 500 micro seconds between flushes
     control.wait_micros(500)
 led_count = 0
+return_val = 0
 pins.spi_frequency(1000000)
 pins.spi_format(8, 3)
 
+# WS2801 LED strip  : definition : microbit
+# white             : GND        : GND
+# blue              : MOSI       : P15
+# green             : SCK        : P13
+# red               : VCC        : Not Connected
 def on_forever():
     global led_count
-    led_count = 5
-    light_on(led_count, 5, 5, 0)
+    led_count = 4
+    light_on2(led_count, 0, 5, 0)
     basic.clear_screen()
     basic.pause(1000)
-    light_on(led_count, 8, 8, 8)
+    light_on2(led_count, 0, 0, 5)
     basic.show_icon(IconNames.HEART)
     basic.pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
 function light_on (number_leds: number, red: number, green: number, blue: number) {
     for (let index = 0; index < number_leds; index++) {
-        pins.spiWrite(red)
-pins.spiWrite(green)
-pins.spiWrite(blue)
+        return_val = pins.spiWrite(red)
+        return_val = pins.spiWrite(green)
+        return_val = pins.spiWrite(blue)
     }
+    // WS2801 needs 500 micro seconds between flushes
     control.waitMicros(500)
 }
 let led_count = 0
+let return_val = 0
 pins.spiFrequency(1000000)
 pins.spiFormat(8, 3)
+let total_number_leds = 96
+light_on(total_number_leds, 0, 0, 0)
+// WS2801 LED strip  : definition : microbit
+// white             : GND        : GND
+// blue              : MOSI       : P15
+// green             : SCK        : P13
+// red               : VCC        : Not Connected
 basic.forever(function () {
-    led_count = 5
-    light_on(led_count, 5, 0, 0)
+    led_count = 4
+    light_on(led_count, 0, 5, 0)
     basic.clearScreen()
     basic.pause(1000)
     light_on(led_count, 0, 0, 5)
